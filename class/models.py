506f856f2dab8642eb0261f2aa09cbcbed376bb8
Mariana Popirlan <marianapopirlan@gmail.com>
2023-05-17 18:35:47 +0300

    modified  templates/member/details.html

--- class/models.py
+++ class/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Class(models.Model):
+    id = models.CharField(max_length=13, primary_key=True)
+    name = models.CharField(max_length=30)
+    date = models.DateTimeField("Date")
+    trainer = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + " cu " + self.trainer
--- class/views.py
+++ class/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .models import Class
+
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+
+import datetime
+
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
+
+from trainer.models import Trainer
+from .models import Class
+
+from .forms import ClassForm
+
+from django.shortcuts import render, redirect
+
+
+class ClassCreateView(CreateView):
+    template_name = 'class/create_class.html'  # scriem calea catre pagina html unde vom gasi formularul generat
+    model = Class
+    # fields = '__all__' # preia toate fielduril din model si in interfata va fi in ordinea declarata in models.py
+    form_class = ClassForm
+    success_url = reverse_lazy('index')
+
+
+def index(request):
+    class_list = Class.objects.order_by("-date")
+    context = {
+        "class_list": class_list,
+    }
+    return render(request, "class/index.html", context)
+
+
+def class_details(request, id):
+    class_obj = get_object_or_404(Class, pk=id)
+    context = {"class_var": class_obj}
+    return render(request, "class/details.html", context)
+
+
+def class_remove(request, id):
+    class_obj = get_object_or_404(Class, pk=id)
+    class_obj.delete()
+    return redirect("/class/")
